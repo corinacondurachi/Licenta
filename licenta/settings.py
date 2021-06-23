15f64f0218905a1de0bcf34c3320138c3b7d0a81
corinacondurachi <45423375+corinacondurachi@users.noreply.github.com>
2021-06-23 16:01:44 +0300

    added link

--- licenta/settings.py
+++ licenta/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-twgd#(tq4-f5snpr!wyosf%scqek55yh_*_6-nn+i_f%my35b+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'automaticcolorization.herokuapp.com']
 
 
 # Application definition
